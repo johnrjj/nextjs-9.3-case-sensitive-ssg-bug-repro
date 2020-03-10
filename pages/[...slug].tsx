d385439bc959a9ca87e71b0ae33bc8e7115d89c8
John Johnson <johnrjj@gmail.com>
2020-03-09 23:22:05 -0700

    Created repro

---
+++ pages/[...slug].tsx
@@ -0,0 +1,38 @@
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { slug: ["FOO"] } }],
+    fallback: true
+  };
+}
+
+export async function getStaticProps({
+  params
+}: {
+  params: {
+    slug?: Array<string>;
+  };
+}) {
+  return {
+    props: {
+      slugIdx1: params?.slug?.[0] ?? null,
+      slugIdx2: params?.slug?.[1] ?? null
+    }
+  };
+}
+
+const Foo = () => {
+  return (
+    <>
+      <div>
+        Hello! Deploy me to Now, and if you try to access /foo, it will fail the
+        first time with an unexpected error
+      </div>
+      <div>
+        However, if you deploy to now, and try to access /FOO, it will work the
+        first time
+      </div>
+    </>
+  );
+};
+
+export default Foo;
